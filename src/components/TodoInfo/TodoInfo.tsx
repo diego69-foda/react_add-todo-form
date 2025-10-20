28dbf08046e8573cf8a69e8894ce539ea75ff9be
diego69-foda <diego.bkuhn@gmail.com>
2025-10-20 14:08:25 -0300

    Refactor TodoInfo component for improved readability and consistency

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,29 +1,27 @@
 export const TodoInfo = () => {
-    <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
+  <section className="TodoList">
+    <article data-id="1" className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">delectus aut autem</h2>
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
 
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
+    <article data-id="15" className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">delectus aut autem</h2>
 
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
 
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
+    <article data-id="2" className="TodoInfo">
+      <h2 className="TodoInfo__title">quis ut nam facilis et officia qui</h2>
 
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+        Patricia Lebsack
+      </a>
+    </article>
+  </section>;
 };
